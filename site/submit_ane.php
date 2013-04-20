728db252c38e5f9999c508adf430b1b8917ef526
vcapel <vcapel.capel_v@yahoo.com>
2013-04-21 00:06:05 +0300

    adaugat windspeed

---
+++ site/submit_ane.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!isset($_GET['spd']))
+{echo "NOK!";
+exit;}
+
+$adresa_db = "localhost:3306";
+$utilizator_db = "root";
+$parola_db = "hackathon";
+$db = mysql_connect($adresa_db, $utilizator_db, $parola_db) or die("Database unavailable!");
+mysql_select_db("hackathon", $db);
+
+$temp="INSERT anemometru
+			SET speed=\"$_GET[spd]\";";
+		
+		$query = mysql_query($temp);
+		if (!$query) {
+			$message  = 'Database error. Please try again.';
+			die($message);
+			exit;
+		}
+
+echo "OK";
+
+?>
---
+++ site/test_ane.php
@@ -0,0 +1,27 @@
+<?php
+$adresa_db = "localhost:3306";
+$utilizator_db = "root";
+$parola_db = "hackathon";
+$db = mysql_connect($adresa_db, $utilizator_db, $parola_db) or die("Database unavailable!");
+mysql_select_db("hackathon", $db);
+
+$temp=" SELECT *
+		FROM anemometru
+		ORDER BY utc DESC
+		LIMIT 20";
+		$query = mysql_query($temp);
+		if (!$query) {
+			$message  = 'Database error. Please try again.';
+			die($message);
+			exit;
+		}
+echo "<TABLE><TR><B><TD>Speed</TD><TD>UTC</TD></B></TR>";	
+while ($row = mysql_fetch_assoc($query)) 
+	{
+		echo "<TR><TD>$row[speed]</TD><TD>$row[UTC]</TD></TR>";	
+	}
+echo "</TABLE>";	
+
+mysql_free_result($query);
+
+?>
